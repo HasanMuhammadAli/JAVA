03c284d64e0256c38f171cf2d0d4697c624203d0
HasanMuhammadAli <mali.hasan.612@gmail.com>
2024-12-28 16:43:02 +0530

    And

---
+++ And.java
@@ -0,0 +1,15 @@
+public class And {
+    
+    public static void main(String[] args) {
+        int age = 56;
+        double account_balance = 1050600.84;
+        if (account_balance >= 1000000 && age >= 60){
+            System.out.println("You can retire now, Good luck.");
+        }
+        else{
+            System.out.println("You still need to save moneey for retirement.");
+        }
+    }
+    
+    
+}
