fb95d472a52491e87c62279e4ec332a1b5d9a360
HasanMuhammadAli <mali.hasan.612@gmail.com>
2024-12-28 16:40:45 +0530

    Or

---
+++ Or.java
@@ -0,0 +1,12 @@
+public class Or {
+    public static void main(String[] args) {
+        int age = 56;
+        double account_balance = 1050600.84;
+        if (account_balance >= 1000000 || age >= 65){
+            System.out.println("You can retire now, Good luck.");
+        }
+        else{
+            System.out.println("You still need to save moneey for retirement.");
+        }
+    }
+}
