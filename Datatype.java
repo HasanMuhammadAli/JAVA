f44c75ed0fe2fa10b1b5670b599ffa274c9f595b
HasanMuhammadAli <mali.hasan.612@gmail.com>
2024-12-28 13:08:13 +0530

    Datatype

---
+++ Datatype.java
@@ -0,0 +1,22 @@
+public class Datatype {
+    public static void main(String[] args){
+        byte b1 = 21;   //byte datatype
+        short shortNumber = 241;    //byte datatype
+        int i1 = 25;    //integer datatype
+        long longNumber =1313;  //long datatype
+        float floastingNumber = 241.35f; //float datatype
+        double balance = 1210.54;   //double datatype
+        boolean answer = true;  //boolean datatype
+        char ch = 'A';  //character datatype
+
+        System.out.println("Byte number is:" + b1);
+        System.out.println("Short number is:" + shortNumber);
+        System.out.println("Int number is:" + i1);
+        System.out.println("Long number is:" + longNumber);
+        System.out.println("Floatinf value is:" + floastingNumber);
+        System.out.println("Doble value is:" + balance);
+        System.out.println("Boolean value is:" + answer);
+        System.out.println("Char value is:" + ch);
+
+    }
+}
