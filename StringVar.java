8ed4a0768dd74029e36ce603a4c53d10983c9268
HasanMuhammadAli <mali.hasan.612@gmail.com>
2024-12-28 12:50:25 +0530

    String

---
+++ StringVar.java
@@ -0,0 +1,7 @@
+public class StringVar {
+    public static void main(String[] args) {
+        String name="Ali";
+        System.out.println("My name is "+name+".");
+        System.out.println("I love node.js.");
+    }
+}
