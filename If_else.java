c863f7cca069d56d53d87eaf67bd8f590397b9a0
HasanMuhammadAli <mali.hasan.612@gmail.com>
2024-12-28 16:08:24 +0530

    If_else

---
+++ If_else.java
@@ -0,0 +1,11 @@
+public class If_else {
+    public static void main(String[] args){
+        String weather = "raining";
+        if(weather.equals("raining")){
+            System.out.println("I am staying home.");
+        }
+        else{
+            System.out.println("I am going out for walk.");
+        }
+    }
+}
