179854dc855c77e3aad31fc50c7906f5db17ab13
HasanMuhammadAli <mali.hasan.612@gmail.com>
2024-12-28 16:13:56 +0530

    if_else2

---
+++ If_else2.java
@@ -0,0 +1,11 @@
+public class If_else2 {
+    public static void main(String[] args) {
+        boolean answer = false;
+        if(answer == true){
+            System.out.println("The answer is true");
+        }
+        else{
+            System.out.println("The answer is false");
+        }
+    }
+}
