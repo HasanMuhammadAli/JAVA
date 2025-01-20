0a965483b3776f713f914bb04822223541656709
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-01-20 23:02:02 +0530

    eedited

---
+++ switchcase.java
@@ -0,0 +1,24 @@
+
+public class switchcase {
+    public static void main(String[] args){
+        String grade = "FF";
+        switch(grade){
+            case "AA":
+                System.out.println("Great Student");
+                break;
+            case "AB":
+                System.out.println("Great Student");
+                break;
+            case "BB": case "BC" :
+                System.out.println("Average Student");
+                break;
+            case "CC": case "CD" : case "DD":
+                System.out.println("Poor Student");
+                break;
+            default:
+                System.out.println("Failed Student");
+                
+        }
+
+    }
+}
