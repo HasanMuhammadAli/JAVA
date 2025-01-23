80e33a2b14b499d00753a9de315d14a8b74f4e40
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-01-23 20:38:22 +0530

    loop added

---
+++ Array_Loop.java
@@ -0,0 +1,9 @@
+public class Array_Loop {
+    public static void main(String []args){
+        int ages[]={8,14,18,37,42};
+        //In below loop [age get particular value from above array] each time.
+        for(int age:ages){
+            System.err.println(age);
+        }
+    }
+}
