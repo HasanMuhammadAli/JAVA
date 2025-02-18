82fbce32424c06d49844edd23091e029571fdd19
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-02-18 16:08:12 +0530

    EvenOdd added

---
+++ NormalProg/EvenOdd.java
@@ -0,0 +1,17 @@
+package NormalProg;
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even.");
+        } else {
+            System.out.println(num + " is Odd.");
+        }
+    }
+}
