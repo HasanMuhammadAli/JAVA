1b636613ad1481f953b9a5c1f0222d5eb8d58def
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-02-22 22:52:17 +0530

    Fibonacci seq code added

---
+++ NormalProg/Fibonacci.java
@@ -0,0 +1,19 @@
+package NormalProg;
+import java.util.Scanner;
+//Program for fibonacci sequence
+class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = scanner.nextInt();
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: " + a + " " + b);
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+        scanner.close();
+    }
+}
