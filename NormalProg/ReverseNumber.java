340e016aa5bb13b53845f12d8b62a8c2f30de246
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-02-23 22:05:48 +0530

    ReverseNumber code added

---
+++ NormalProg/ReverseNumber.java
@@ -0,0 +1,16 @@
+package NormalProg;
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int rev = 0;
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        System.out.println("Reversed: " + rev);
+        scanner.close();
+    }
+}
