e604bd1f82b7563046f5dbe618f33f170589f6e6
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-03-12 21:42:54 +0530

    ArithmeticException added

---
+++ ExceptionHandling/Ex01.java
@@ -0,0 +1,31 @@
+package ExceptionHandling;
+
+class Ex01 {
+    public static void main(String[] args) {
+        int n = 10;
+        int m = 0;
+
+        //int ans = n / m;
+        //System.out.println("Answer: " + ans);
+
+        /*
+           Below error occurs when we divide by zero.
+
+         * Exception in thread "main" java.lang.ArithmeticException: / by zero
+           at ExceptionHandling.DivideZero.main(DivideZero.java:8)
+         */
+
+         // Java program to demonstrates handling the exception using try-catch block
+
+        try{
+            // Code that may throw an exception
+            int ans = n / m;   
+            System.out.println("Answer: " + ans);
+        }catch(ArithmeticException err){
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+        finally{
+            System.out.println("Program continues after handling the exception.");
+        }
+    }
+}
