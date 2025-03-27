a826a3b19b149f0aa7e8fe512ad3cd5fc48fc740
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-03-27 16:10:53 +0530

    bytestream1 added

---
+++ FileIO/ByteStream.java
@@ -0,0 +1,20 @@
+package FileIO;
+import java.io.FileReader;
+import java.io.IOException;
+
+class ByteStream {
+    public static void main(String[] args) throws IOException{
+        FileReader in = null;
+        try{
+            in = new FileReader("/Users/AliHasan/sem4/JA/FileIO/input.txt");
+            int i = 0;
+            while ((i = in.read()) != -1) {
+                System.out.print((char)i);
+            }
+        }finally{
+            if(in != null){
+                in.close();
+            }
+        }
+    }
+}
---
+++ FileIO/Readme.txt
@@ -0,0 +1,6 @@
+File Name            Workdone init
+----------------------------------------
+ByteStream           Read a File
+ByteStream2          Read and write a File
+CharacterStream      Read a File
+CharacterStream2     Read and write a File
